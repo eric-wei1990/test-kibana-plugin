ed8cd67397c8ab1806a078ac10de5c803e62554a
weizenghui <zenghui.wei@navercorp.com>
2021-01-21 17:03:13 +0800

    init kibana plugin

---
+++ public/application.tsx
@@ -0,0 +1,27 @@
+import React from 'react';
+import ReactDOM from 'react-dom';
+import { AppMountParameters, CoreStart } from '../../../src/core/public';
+import { AppPluginStartDependencies } from './types';
+import { TestPluginApp } from './components/app';
+import { Storage } from '../../../src/plugins/kibana_utils/public';
+
+const local = new Storage(window.sessionStorage);
+console.log(local);
+
+export const renderApp = (
+  { notifications, http }: CoreStart,
+  { navigation }: AppPluginStartDependencies,
+  { appBasePath, element }: AppMountParameters
+) => {
+  ReactDOM.render(
+    <TestPluginApp
+      basename={appBasePath}
+      notifications={notifications}
+      http={http}
+      navigation={navigation}
+    />,
+    element
+  );
+
+  return () => ReactDOM.unmountComponentAtNode(element);
+};
